9d6403bf7650775697e5c51df549367087ab8266
zarehba <zarehba@protonmail.com>
2020-06-14 20:23:49 +0200

    export  Button styles allowing them to be reused

--- src/shared/Button.js
+++ src/shared/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
+export const StyledButton = styled.button`
   display: block;
   padding: 1rem;
   border-radius: 5px;
